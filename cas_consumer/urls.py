b441531fc622f604c32cae697b8c4669f8dc0907
Alexandre Marinho <lyralemos@gmail.com>
2016-06-23 09:15:05 -0300

    Erro de digitação

--- cas_consumer/urls.py
+++ cas_consumer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from cas_consumer.views import login, logout
 
-urlpatterns[
+urlpatterns = [
     url(r'^login/', login, name="cas_login"),
     url(r'^logout/', logout, name="cas_logout"),
 ]
